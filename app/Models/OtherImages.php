2df8c1ce094d4ce3d409c71da640c9a37fe82b84
ahmed6893 <rifatahmed6893@gmail.com>
2025-05-04 19:56:28 +0600

    Product Crud Done

--- app/Models/OtherImages.php
+++ app/Models/OtherImages.php
@@ -8,7 +8,7 @@ class OtherImages extends Model
 {
     public static $newImages ,$newImage ,$productImages ,$image,$imageName,$directory, $extension ,$imageUrl;
 
-    public static function productOtherImages($productId ,$images)
+    public static function productOtherImages($images,$productId)
     {
         foreach($images as $image){
             self::$imageUrl = self::getImageUrl($image);
@@ -19,6 +19,15 @@ class OtherImages extends Model
             self::$productImages->save();
         }
     }
+    public static function getImageUrl($image)
+    {
+        self::$extension = $image->extension();
+        self::$imageName = rand(000,999).'.'.self::$extension;
+        self::$directory ='admin/images/otherImages/';
+        $image->move(self::$directory,self::$imageName);
+        self::$imageUrl = self::$directory.self::$imageName;
+        return self::$imageUrl;
+    }
     public static function updateOtherImages ($images ,$id)
     {
         self::$newImages = OtherImages::where('productId',$id)->get();
@@ -28,7 +37,7 @@ class OtherImages extends Model
             {
                 unlink($newImage->image);
             }
-            self::$newImage->delete();
+            $newImage->delete();
         }
 
         foreach($images as $image){
